89561c9e60de7524c6c665d9b482d91d6abc3be7
DigiReaper7 <jonv540i@gmail.com>
2026-09-01 00:10:55 -0500

    Corrected hello.py to show the current date not a baked in one.

--- hello/hello.py
+++ hello/hello.py
@@ -1,5 +1,5 @@
 import datetime as dt
 name = input("What is your name? ")
-d = dt.date.fromordinal(739859)
-d = d.strftime("%A, %B %d, %Y")
-print("Hi", name, "It's", d)
+today = dt.date.today()
+today = today.strftime("%A, %B %d, %Y")
+print("Hi", name, "It's", today)
