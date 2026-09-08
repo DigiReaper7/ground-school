fe9c785d9f11542def4622765d295f342fd979f5
DigiReaper7 <jonv540i@gmail.com>
2026-09-07 22:58:14 -0500

    Code adjusted to reduce bugs related to using floats as ints.

--- week2/battery_triage.py
+++ week2/battery_triage.py
@@ -2,17 +2,17 @@ volts = float(input("What is your cell's voltage? "))
 
 if volts <= 3.00:
     print(f"DANGER - @ {volts:.2f} volts. Better hope it still works!!!")
-elif volts >= 3.01 and volts <= 3.60:
+elif volts <= 3.60:
     print(f"Discharged to {volts:.2f} volts. Recharge to a safe level.")
-elif volts >= 3.61 and volts <= 3.84:
+elif volts <= 3.84:
     print(f"@ - {volts:.2f} volts. Recharge needed.")
-elif volts == 3.85:
+elif volts <= 3.85:
     print(f"@ - {volts:.2f} volts. Ready for storage.")
-elif volts >= 3.86 and volts <= 4.19:
+elif volts <= 4.19:
     print(f"@ - {volts:.2f} volts. Recharge to 4.2V")
-elif volts == 4.20:
+elif volts <= 4.20:
     print(f"@ - {volts:.2f} volts. Ready to rock n' roll!!!")
-elif volts >= 4.21 and volts <= 4.5:
-    print(f"DANGER @ - {volts:.2f} VOLTS!!! DANGER - DISCHARGE THIS MOTHER FUCKER TO A SAFE LEVEL NOW!!!")
+elif volts <= 4.5:
+    print(f"DANGER @ - {volts:.2f} VOLTS!!! DANGER - DISCHARGE TO A SAFE LEVEL NOW!!!")
 else:
     print("Bruh...for real?!?!?!")
