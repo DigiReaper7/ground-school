f7da0c0c5f04ca802388d17c6b6ba360653d6477
DigiReaper7 <jonv540i@gmail.com>
2026-09-07 23:36:46 -0500

    Minor code edits to tighten up readings.

--- week2/battery_triage.py
+++ week2/battery_triage.py
@@ -1,8 +1,8 @@
 volts = float(input("What is your cell's voltage? "))
 
-while volts < 2.0 or volts > 4.5:
+while volts < 0 or volts > 4.5:
     print(f"Try again friend. Not a valid cell voltage.")
-    volts = float(input("What is your cell's voltage."))
+    volts = float(input("What is your cell's voltage?"))
 
 if volts <= 3.00:
     print(f"DANGER - @ {volts:.2f} volts. Better hope it still works!!!")
